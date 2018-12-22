e1d13cfc7f200afe0cab4702f2c143f0fce17a67
Matthew Russell <mrussell@neptec.com>
2018-12-22 22:58:57 +0000

    Created melodic recipe

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 Conan recipe to build ROS's TF2 library.  This script should work on all platforms.
 
-Note that it requires ninja_installer as a build requirement on Windows.  Also, it has external dependencies (so on days where ROS installer won't download things, this recipe fails.)
+# Introduction
+
+This is designed to be a _ROS Independent_ build of TF2.  It was built by setting up an 18.04 VM, following the ROS install instructions, cloning the [ros/geometry2](https://github.com/ros/geometry2) repository, and copying in the required files from the apt install.
+
+Specifically:
+- Followed [these](http://wiki.ros.org/melodic/Installation/Ubuntu) instructions. _e.g._ added the ppa, and installed `ros-melodic-ros-base`
+- Cloned [geomtry2]https://github.com/ros/geometry2) at tag `0.6.5` / `39b89b987e1333d39557ad0555c96214cea17dc8`
+- Manually copied the `ros`, `geometry_msgs`, `tf2_msgs`, and `std_msgs` includes installed to the system into `include/`
+- Re-wrote the `CMakeLists.txt` file
+- Authored the `conanfile.txt` to provide boost and `console_bridge`
+
+# Updating Source
+
+If the tf2 source needs to be updated, _e.g._ if the `melodic-develop` is updated, you should be able to clone the `geometry2` repository, filter tf2 out, and pull the updates in.
+
+```sh
+cd /tmp
+git clone https://github.com/ros/geometry2
+cd geometry2
+git filter-branch --subdirectory-filter tf2 -- --all
+
+git clone https://github.com/kheaactua/conan-tf2.git
+git checkout melodic
+git remote add updates file:///tmp/geometry2
+
+git pull --allow-unrelated-histories updates melodic-devel
+```
--- conanfile.py
+++ conanfile.py
@@ -1,226 +1,57 @@
 #!/usr/bin/env python
-# -*- coding: future_fstrings -*-
 # -*- coding: utf-8 -*-
 
-import sys, os, glob, platform, re
-from io import StringIO # Python 2 and 3 compatible
-from conans import ConanFile, tools, VisualStudioBuildEnvironment
-from conans.errors import ConanException
-from conans.model.version import Version
+import os
+from conans import ConanFile, CMake, tools
 
 
 class Tf2Conan(ConanFile):
     name             = 'tf2'
-    version          = 'indigo'
+    version          = 'melodic'
     license          = 'Creative Commons Attribution 3.0'
     url              = 'http://wiki.ros.org/tf2'
-    description      = 'tf2 is the second generation of the transform library, which lets the user keep track of multiple coordinate frames over time.'
-    settings         = 'os', 'compiler', 'build_type', 'arch', 'arch_build'
+    description      = 'ROS-independent build of tf2, built by grabbing TF2 files from ros/geometry2/tf2, and supplying the dependencies with files normally installed by the ROS ppa'
+    settings         = 'os', 'compiler', 'build_type', 'arch'
     options          = {'shared': [True, False]}
     default_options  = 'shared=True'
     generators       = 'cmake'
-    ros_install_file = 'indigo-tf2-wet.rosinstall'
     requires = (
-        'boost/[>1.46]@ntc/stable',
-        'console_bridge/indigo@ntc/stable',
-        'gtest/[>=1.8.0]@bincrafters/stable',
-        'helpers/0.2@ntc/stable',
+        'boost/[>1.60]@conan/stable',
+        'console_bridge/master@ntc/stable', # TODO put a version number on this
+        'helpers/0.3@ntc/stable',
     )
     options = {
         'shared': [True, False],
-        'fPIC':   [True, False],
     }
-    default_options = ('shared=True', 'fPIC=True')
-
-    def system_requirements(self):
-        pkg_names = 'rosinstall-generator', 'rosdep', 'wstool', 'rosinstall', 'empy'
-        if pkg_names:
-            try:
-                self.run('pip install -U %s'%' '.join(pkg_names))
-            except ConanException:
-                self.output.warn('Could not run pip updates.  Ensure you have the following pip packages installed: %s'%', '.join(pkg_names))
-
-    def build_requirements(self):
-        try:
-            mybuf = StringIO()
-            self.run('ninja --version', output=mybuf)
-            outp = mybuf.getvalue()
-        except ConanException:
-            self.output.info('ninja not found in path')
-            outp = ''
-
-        m = re.match(r'(?P<version>\d+\.\d+\.\d+)', outp)
-        if not m or Version(str(m.group('version'))) < '1.7.2':
-            self.output.info('Could not detect Ninja which is a build requirement for tf2 on Windows.  Adding ninja_installer@bincrafters.  Detection buffer = "%s"'%outp.strip())
-            self.build_requires('ninja_installer/1.8.2@bincrafters/stable')
+    default_options = ('shared=True')
+    exports_sources = 'include*', 'src*', 'CMakeLists.txt'
 
     def config_options(self):
-        if self.settings.compiler == "Visual Studio":
-            self.options.remove('fPIC')
-        if 'Windows' == platform.system():
+        if 'Visual Studio' == self.settings.compiler:
+            # When shared, VS trips over a bunch of undeclared symbols
             self.options.remove('shared')
 
-    def source(self):
-        if 'Windows' == self.settings.os:
-            python_bin = 'python'
-        else:
-            # On *nix, we shoudn't need to specify this
-            python_bin = ''
-        if len(python_bin):
-            python_bin += ' '
+    def _setup_cmake(self):
+        cmake = CMake(self)
 
-        if 'VIRTUAL_ENV' in os.environ:
-            python_bin_path = os.path.join(os.environ['VIRTUAL_ENV'], 'Scripts' if 'Windows' == platform.system() else 'bin')
-        elif 'PYENV_VIRTUALENV_INIT' in os.environ:
-            # python_bin_path = os.path.join(os.environ['HOME'], '.pyenv', 'shims')
-            python_bin_path = ''
-        else:
-            self.output.warn('This script was written to be run in python\'s virtualenv, running outside of a virtualenv might result in difficulty locating the ROS python tools (such as rosinstall_generator')
-            python_bin_path = '/usr/bin'
-        if len(python_bin_path):
-            python_bin_path += '/'
+        cmake.definitions['BOOST_ROOT:PATH']         = self.deps_cpp_info['boost'].rootpath
+        cmake.definitions['BUILD_SHARED_LIBS:BOOL']  = 'TRUE' if 'shared' in self.options and self.options.shared else 'FALSE'
 
-        cmd =f'{python_bin}{python_bin_path}rosinstall_generator tf2 --rosdistro "{self.version}" --deps --wet-only --tar > "{self.ros_install_file}"'
-        try:
-            self.run(cmd)
-        except ConanException as e:
-            self.output.error('rosinstall_generated failed. cmd=%s\nError: %s'%(cmd, e))
-            if os.path.exists(self.ros_install_file):
-                with open(self.ros_install_file, 'r') as content_file: content = content_file.read()
-                self.output.error('ROS install file contents:\n%s'%content)
-            else:
-                self.output.error('ROS install file "%s" was not created'%self.ros_install_file)
-            sys.exit(-1)
+        cmake.definitions['console_bridge_DIR:PATH'] = os.path.join(self.deps_cpp_info['console_bridge'].rootpath, 'lib', 'console_bridge', 'cmake')
 
-        if 'Linux' == platform.system():
-            ncors = tools.cpu_count()
-        else:
-            # Seems to work better...
-            ncors = 1
-
-        cmd = f'{python_bin}{python_bin_path}wstool init -j {ncors} src "{self.ros_install_file}"'
-        try:
-            self.run(cmd)
-        except ConanException:
-            self.output.error('wstool failed, restarting it.  cmd=%s'%cmd)
-
-            try:
-                self.run(f'{python_bin}{python_bin_path}wstool update -j {ncors} -t src')
-            except ConanException as e:
-                self.output.error('wstool failed: %s'%e)
-                sys.exit(-1)
-
-        # If we're using MSVC, a env.bat file will be generated rather than a
-        # env.sh, update the code to handle this.  The code injected tests for
-        # platform though, not compiler, so maybe it should be hard set to
-        # env.bat if conan is using MSVC?
-        tools.replace_in_file(
-            file_path=os.path.join('src', 'catkin', 'python', 'catkin', 'builder.py'),
-            search="'env.sh'",
-            replace="'env.bat' if 'Windows' == platform.system() else 'env.sh'"
-        )
-
-        # Remove code that would otherwise be removed by macros.h, but isn't
-        # include here for some reason
-        for f in [os.path.join('src', 'geometry2', 'tf2', 'include', 'tf2', 'LinearMath', p) for p in ['Matrix3x3.h', 'Quaternion.h']]:
-            tools.replace_in_file(
-                file_path=f,
-                search='__attribute__((deprecated))',
-                replace=''
-            )
-
-        if 'Windows' == platform.system():
-            # Modify template to remove NO_ERROR name collision with Windows
-            tools.replace_in_file(
-                file_path=os.path.join('src', 'gencpp', 'scripts', 'msg.h.template'),
-                search='#include <ros/message_operations.h>',
-                replace='#include <ros/message_operations.h>\n\n\n#ifdef _WIN32\n#undef NO_ERROR\n#endif\n\n'
-            )
+        return cmake
 
     def build(self):
-        if 'indigo' == self.deps_cpp_info['console_bridge'].version:
-            console_bridge_cmake_path = os.path.join(self.deps_cpp_info['console_bridge'].rootpath, 'share', 'console_bridge', 'cmake')
-        else:
-            console_bridge_cmake_path = os.path.join(self.deps_cpp_info['console_bridge'].rootpath, 'lib', 'console_bridge', 'cmake')
-
-        args = []
-        args.append('-DBOOST_ROOT:PATH=%s'%self.deps_cpp_info['boost'].rootpath)
-        args.append('-DBUILD_SHARED_LIBS:BOOL=%s'%('TRUE' if 'shared' in self.options and self.options.shared else 'FALSE'))
-        args.append(f'-DCMAKE_BUILD_TYPE:STRING={self.settings.build_type}')
-        args.append('-DGTEST_ROOT:PATH=%s'%self.deps_cpp_info['gtest'].rootpath)
-        args.append(f'-Dconsole_bridge_DIR:PATH={console_bridge_cmake_path}')
-        args.append('-DCONAN_CONSOLE_BRIDGE_ROOT:PATH=%s'%self.deps_cpp_info['console_bridge'].rootpath)
-        args.append(f'-DCMAKE_INSTALL_PREFIX={self.package_folder}')
-
-        # Adding this to remove the --install-layout option on Tegra, got the
-        # suggestion from https://github.com/ros/catkin/issues/863
-        args.append('-DSETUPTOOLS_DEB_LAYOUT:BOOL=OFF')
-
-        # Require ninja.  It's good on Linux, and required on MSVC (this just won't build with MSVC)
-        args.append('--use-ninja')
-
-        if self.settings.get_safe('arch').startswith('arm') and 'TOOLCHAIN_ROOT' in os.environ:
-            args.append('-DRT_LIBRARY=%s'%os.path.join(os.environ['TOOLCHAIN_ROOT'], 'libc', 'usr', 'lib', 'librt.so'))
-
-        if self.settings.compiler == "Visual Studio":
-            from platform_helpers import adjustPath
-            for f in ['cpp_common', 'genmsg', 'roscpp_traits', 'std_msgs', 'geometry_msgs', 'message_generation']:
-                args.append('-D%s_DIR=%s'%(f, adjustPath(os.path.join(self.package_folder, 'share', f, 'cmake'))))
-            args.append('-DCMAKE_PREFIX_PATH=%s'%adjustPath(self.package_folder))
-            env_build = VisualStudioBuildEnvironment(self)
-            with tools.environment_append(env_build.vars):
-                vcvars = tools.vcvars_command(self.settings)
-                cmd = '%s && python src/catkin/bin/catkin_make_isolated --install %s'%(vcvars, ' '.join(args))
-                self.output.info(f'Running: {cmd}')
-                self.run(cmd)
-        else:
-            cmd = 'src/catkin/bin/catkin_make_isolated --install %s'%(' '.join(args))
-            self.output.info(f'Running: {cmd}')
-            self.run(cmd)
+        cmake = self._setup_cmake()
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        from platform_helpers import adjustPath
-
-        if 'Windows' == self.settings.os:
-            self.copy(pattern='*.pdb',  dst='bin', keep_path=False)
-
-        # The pkg-config variables are full of absolute paths.
-        p_files = glob.glob(os.path.join(self.package_folder, 'lib', 'pkgconfig', '*.pc'))
-        for f in p_files:
-            self.output.info('Modifying %s'%f)
-
-            tools.replace_in_file(
-                file_path=f,
-                search=adjustPath(self.package_folder),
-                replace=r'${prefix}',
-                strict=False,
-            )
-
-            if 'Linux' == platform.system() and os.path.basename(f) in ['cpp_common.pc', 'tf2.pc', 'rostime.pc']:
-                # Small bug I saw
-                tools.replace_in_file(file_path=f, search=r'-l:', replace=r'-l', strict=False)
-
-            if os.path.basename(f) in ['cpp_common.pc', 'tf2.pc']:
-                tools.replace_in_file(
-                    file_path=f,
-                    search=adjustPath(self.deps_cpp_info['console_bridge'].rootpath),
-                    replace=r'${PKG_CONFIG_CONSOLE_BRIDGE_PREFIX}',
-                    strict=False
-                )
-
-        self.copy(pattern='*.pc',  dst='lib/pkgconfig', src=os.path.join('install_isolated', 'lib', 'pkgconfig'))
+        cmake = self._setup_cmake()
+        cmake.configure()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-
-        # Populate the pkg-config environment variables
-        with tools.pythonpath(self):
-            from platform_helpers import adjustPath, appendPkgConfigPath
-
-            pkgs = ['catkin', 'genmsg', 'gencpp', 'genlisp', 'genpy', 'cpp_common', 'message_generation', 'message_runtime', 'roscpp_traits', 'rostime', 'roscpp_serialization', 'std_msgs', 'actionlib_msgs', 'geometry_msgs', 'tf2_msgs', 'tf2']
-            for p in pkgs:
-                setattr(self.env_info, 'PKG_CONFIG_%s_PREFIX'%p.upper(), adjustPath(self.package_folder))
-
-            appendPkgConfigPath(adjustPath(os.path.join(self.package_folder, 'lib', 'pkgconfig')), self.env_info)
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
